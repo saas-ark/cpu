d19615fbe6d5b0a6effbbbd9fc4ea984c02da0ac
saas-ark <bwubta22049@brainwareuniversity.ac.in>
2024-09-06 09:58:30 +0530

    Create proc.py

---
+++ proc.py
@@ -0,0 +1,11 @@
+# Using a temporary variable
+x = 5
+y = 10
+
+# Swapping the values
+temp = x
+x = y
+y = temp
+
+print("The value of x after swapping: {}".format(x))
+print("The value of y after swapping: {}".format(y))
